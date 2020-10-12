ed33b0dfce89eefbba2a5bedb70b015835f64f8b
Kalelovil <kalelovil@gmail.com>
2020-10-13 11:11:57 +1300

    - unit factory placeholder

---
+++ Assets/UnitFactory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitFactory : MonoBehaviour
+{
+    #region Production
+    [SerializeField] NodeUnit.UnitType _unitTypeProduced;
+
+    [SerializeField] int _unitsPerDay;
+    #endregion
+
+    #region Time
+    private void OnEnable()
+    {
+        DateManager.CurrentHourChangedAction += CurrentHourChanged;
+        DateManager.CurrentDayChangedAction += CurrentDayChanged;
+    }
+
+    private void OnDisable()
+    {
+        DateManager.CurrentHourChangedAction -= CurrentHourChanged;
+        DateManager.CurrentDayChangedAction -= CurrentDayChanged;
+    }
+
+    private void CurrentDayChanged(int dayNum)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void CurrentHourChanged(int hourNum)
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
